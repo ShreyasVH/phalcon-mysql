4571cd20a9009818891d2e567b5590543a8860ce
ShreyasVH <shreyas.deve.1@gmail.com>
2023-05-18 20:45:27 +0530

    initial commit

---
+++ app/config/loader.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phalcon\Autoload\Loader;
+
+$loader = new Loader();
+
+$loader->setDirectories
+(
+    array
+    (
+        APP_PATH . 'app/controllers/',
+        APP_PATH . 'app/models/',
+        APP_PATH . 'app/repositories/'
+    )
+
+)->register();
+
+$loader->setNamespaces
+(
+    array
+    (
+        'app\\controllers' => APP_PATH . 'app/controllers',
+        'app\\config' => APP_PATH . 'app/config',
+        'app\\models' => APP_PATH . 'app/models',
+        'app\\repositories' => APP_PATH . 'app/repositories'
+    )
+
+)->register();
